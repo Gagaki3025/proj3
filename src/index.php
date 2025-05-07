2e87f44f3dc958cee14ba0864a0f7ef1aa5b53d3
Gagaki3025 <troiskyivan@gmail.com>
2025-05-07 13:30:42 +0300

    php-fpm + php-httpd

--- src/index.php
+++ src/index.php
@@ -1,6 +1,38 @@
 <?php
 
-echo "11111231";
+ phpinfo();
+
+ echo "2111";
+
+// PHP или Apache в httpd может достучаться до php по:
+// http://proj3-php8.2-fpm:9000
+
+// Maildev по SMTP:
+// smtp://proj3-maildev:1025
+
+
+ $memcached = new Memcached();
+ $memcached->addServer('proj3-memcached', 11211);
+ $memcached->set('key', 'value');
+// var_dump($memcached);
+
+
+/*
+$mail = new PHPMailer\PHPMailer\PHPMailer();
+$mail->isSMTP();
+$mail->Host = 'proj3-maildev'; // имя контейнера!
+$mail->Port = 1025;
+$mail->SMTPAuth = false;
+$mail->setFrom('igor@aviaparts.kiev.ua');
+$mail->addAddress('igor@aviaparts.kiev.ua');
+$mail->Subject = 'Тест';
+$mail->Body = 'Сообщение';
+$mail->send();
+*/
+
+
+
+
 
 
 ?>
